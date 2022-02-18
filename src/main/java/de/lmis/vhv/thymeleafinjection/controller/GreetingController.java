be7df0e588a5f20117e1bc596848a14a9303970c
Dominik Lübbers <dominik@epsilonsoft.de>
2022-02-18 08:40:20 +0100

    Fertigstellung

--- src/main/java/de/lmis/vhv/thymeleafinjection/controller/GreetingController.java
+++ src/main/java/de/lmis/vhv/thymeleafinjection/controller/GreetingController.java
@@ -4,8 +4,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.safety.Safelist;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
@@ -15,12 +15,6 @@ public class GreetingController {
         String safeDuties = Jsoup.clean(emphasizedDuties, Safelist.basic());
         model.addAttribute("name", name);
         model.addAttribute("duties", safeDuties);
-        return "greeting";
-    }
-
-    @GetMapping("/greeting")
-    public String getGreeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
         return "greeting";
     }
 
